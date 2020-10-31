c51058e685fc29a7dc1619c461a53cc7ae5dc3e5
Keith <keithtmccartney@hotmail.com>
2020-10-31 15:59:13 +0000

    Completely removing the commented-out code from the last commit.

--- super-duper-octo-fortnight/super_duper_octo_fortnight/Server/Models/ShoppingDBContext.cs
+++ super-duper-octo-fortnight/super_duper_octo_fortnight/Server/Models/ShoppingDBContext.cs
@@ -18,15 +18,6 @@ namespace super_duper_octo_fortnight.Server.Models
         public virtual DbSet<ItemDetails> ItemDetails { get; set; }
         public virtual DbSet<ShoppingDetails> ShoppingDetails { get; set; }
 
-        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=ShoppingDB;Trusted_Connection=True;MultipleActiveResultSets=True");
-            }
-        }*/
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ItemDetails>(entity =>
